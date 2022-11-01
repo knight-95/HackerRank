1574b0dfde07707c4855cdf732cca2d99776f947
Yash Gupta <yashgupta13.in@gmail.com>
2022-11-01 17:21:27 +0530

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(longestEven("hey"));
+//        System.out.println(longestEven("hey"));
+        int[] nums = {31,15,7,3,2};
+        List <Integer> list = new ArrayList<Integer>();
+        list.add(31);
+        list.add(15);
+        list.add(7);
+        list.add(3);
+        list.add(2);
+
+
+//        sortByBits(nums);
+        cardinalitySort(list);
     }
     public static String longestEven(String sentence){
         String longestWord="";
@@ -21,9 +35,23 @@ public class Main {
         }
         return longestWord;
     }
-    public int[] sortByBits(int[] arr) {
+    public static int[] sortByBits(int[] arr) {
         Integer[] wrappedArr = Arrays.stream(arr).boxed().toArray(Integer[]::new);
         Arrays.sort(wrappedArr, (a, b) -> Integer.bitCount(a) == Integer.bitCount(b) ? a - b : Integer.bitCount(a) - Integer.bitCount(b));
+        Arrays.stream(wrappedArr).mapToInt(Integer::intValue).toArray();
+
+        for(int i=0;i< wrappedArr.length;i++){
+            System.out.println(wrappedArr[i]);
+        }
         return Arrays.stream(wrappedArr).mapToInt(Integer::intValue).toArray();
+    }
+
+    public static List<Integer> cardinalitySort(List<Integer> nums){
+        Collections.sort(nums, (a, b) -> Integer.bitCount(a) == Integer.bitCount(b) ? a - b : Integer.bitCount(a) - Integer.bitCount(b));
+
+        for(int i=0;i< nums.size();i++){
+            System.out.println(nums.get(i));
+        }
+        return nums;
     }
 }
